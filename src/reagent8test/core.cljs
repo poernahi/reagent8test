51ec9319baddfa19bcbffb9cb6dcc1f472747f1e
Hendrik Poernama <poernahi@gmail.com>
2017-09-29 13:50:54 +0700

    Initial commit

---
+++ src/reagent8test/core.cljs
@@ -0,0 +1,19 @@
+(ns reagent8test.core
+    (:require [reagent.core :as r]))
+
+(enable-console-print!)
+
+(println "This text is printed from src/reagent8test/core.cljs. Go ahead and edit it and see reloading in action.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
+
+(r/render [:h1 "Works!!"]  (.getElementById js/document "app"))
